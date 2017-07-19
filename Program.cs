bef28d603e99a1fa5f597c7750ce33d897251fdf
jonathan <jandrewswcyt@gmail.com>
2017-07-19 14:17:34 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text; 
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        PrintStairCase(n); 
+    }
+    
+        static void PrintStairCase(int size)
+        {
+            int totalN = size; 
+            while(size > 0)
+            {
+                DrawStairCase(--size, totalN);
+            }
+        }
+
+        static void DrawStairCase(int numPrecedingSpaces, int n,  char pic = '#')
+        {
+            int totalPics = n - numPrecedingSpaces; 
+            StringBuilder sBuilder = new StringBuilder(); 
+
+            for(int i = 0; i < numPrecedingSpaces; i++)
+            {
+                sBuilder.Append(' '); 
+            }
+            
+            for(int i = 0; i < totalPics; i++)
+            {
+                sBuilder.Append(pic);
+            }
+            
+            Console.WriteLine(sBuilder.ToString()); 
+        }
+}
